0fbf7a053fca423a6246a44af7e4cfcd208a0b7d
G584832025 <258483@st.takushoku-u.ac.jp>
2026-05-01 12:38:16 +0900

    諸々の更新

---
+++ 2年前期授業/4-2プログラミング基礎2/授業内課題/P0430A.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class P0430A{
+
+    public static void main(String[] args){
+        Scanner Sc=new Scanner(System.in);
+        String[] A={"zero","one","two","three","four","five","six","eight","nine"};
+        String b=A[Sc.nextInt()];
+        System.out.println(b);
+        
+    }
+}
