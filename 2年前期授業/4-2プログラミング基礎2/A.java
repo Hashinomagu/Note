c350cf27a1f2a2a4694650ddc083b293b49997ef
G584832025 <258483@st.takushoku-u.ac.jp>
2026-06-04 12:20:19 +0900

    プロ２途中の休憩

---
+++ 2年前期授業/4-2プログラミング基礎2/A.java
@@ -0,0 +1,12 @@
+//mainのクラス（実行するクラス）
+class A{
+    public static void main(String[] args){
+        Bunsuu b1=new Bunsuu(5,3); 
+        Bunsuu b2=new Bunsuu(1,2);
+        b1.println();
+
+
+    }
+}
+
+
---
+++ 2年前期授業/4-2プログラミング基礎2/Bunsuu.java
@@ -0,0 +1,12 @@
+// Bunsuuクラス（設計図のクラス)
+public class Bunsuu{
+    int k;
+    int n;
+    Bunsuu(int kk, int nn){
+        k=kk;
+        n=nn;
+    }
+    void println(){
+        System.out.println(k+"/"+n);
+    }
+}
